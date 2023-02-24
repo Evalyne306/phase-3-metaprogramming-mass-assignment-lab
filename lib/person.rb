971f1a72496d8ca3fe644c024a0e848fde2b7115
Evalyne306 <evalyne.wambui@student.moringaschool.com>
2023-02-24 21:47:43 +0300

    done

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,21 @@
 class Person
-  # your code here
+  attr_reader :name, :birthday, :hair_color, :eye_color, :height, :weight,
+              :handed, :complexion, :t_shirt_size, :wrist_size,
+              :glove_size, :pant_length, :pant_width
+
+  def initialize(info)
+    @name = info[:name]
+    @birthday = info[:birthday]
+    @hair_color = info[:hair_color]
+    @eye_color = info[:eye_color]
+    @height = info[:height]
+    @weight = info[:weight]
+    @handed = info[:handed]
+    @complexion = info[:complexion]
+    @t_shirt_size = info[:t_shirt_size]
+    @wrist_size = info[:wrist_size]
+    @glove_size = info[:glove_size]
+    @pant_length = info[:pant_length]
+    @pant_width = info[:pant_width]
+  end
 end
